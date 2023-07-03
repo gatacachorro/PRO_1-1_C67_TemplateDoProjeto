ef7351c3f99c15cf72f71d6fafbb1238a0cd4059
gatacachorro <100207223+gatacachorro@users.noreply.github.com>
2023-07-03 18:43:02 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,15 @@
 import firebase from 'firebase';
 
 // adicione SDK do Firebase
-var firebaseConfig = {
-
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAdEQarkomy2YVW6C-sCR5EIQ6uB0_uLzc",
+  authDomain: "pro-67-594c4.firebaseapp.com",
+  databaseURL: "https://pro-67-594c4-default-rtdb.firebaseio.com",
+  projectId: "pro-67-594c4",
+  storageBucket: "pro-67-594c4.appspot.com",
+  messagingSenderId: "1034617485560",
+  appId: "1:1034617485560:web:939853eeb536c9c762630d"
 };
 // Initialice o Firebase
 firebase.initializeApp(firebaseConfig);
